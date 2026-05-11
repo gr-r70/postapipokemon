6cda5b8580d38b3b5a90526cfcc953630f9e2fd6
gr-r70 <german_ren.rojas@uao.edu.co>
2026-05-11 00:16:11 -0500

    Instanciando la calse logica de pokemonapi

---
+++ Pokemonapi/src/main/java/com/mycompany/pokemonapi/Pokemonapi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pokemonapi;
+
+/**
+ *
+ * @author Windows 11
+ */
+public class Pokemonapi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
